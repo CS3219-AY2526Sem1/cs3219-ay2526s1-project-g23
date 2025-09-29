3299a5eb247af8d6ce2cf59d7b6d520868936a27
delonlee01 <delonlee2001@hotmail.com>
2025-09-29 15:15:10 +0800

    Add sign up page UI

--- peerprep/src/pages/SignUp.tsx
+++ peerprep/src/pages/SignUp.tsx
@@ -10,40 +10,50 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="text-center">
-        <CardTitle className="text-lg">Login to PeerPrep</CardTitle>
-        <CardDescription>Ace your interviews with PeerPrep</CardDescription>
+        <CardTitle className="text-lg">Create a PeerPrep account</CardTitle>
+        <CardDescription>Fill in the form below</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="flex flex-col gap-6">
             <div className="grid gap-3">
-              <Label htmlFor="email">Username or Email</Label>
+              <Label htmlFor="email">
+                <span>
+                  Username <span className="text-red-500">*</span>
+                </span>
+              </Label>
               <Input type="text" required />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="password">Password</Label>
+              <Label htmlFor="email">
+                <span>
+                  Email <span className="text-red-500">*</span>
+                </span>
+              </Label>
+              <Input type="email" placeholder="example@gmail.com" required />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="password">
+                <span>
+                  Password <span className="text-red-500">*</span>
+                </span>
+              </Label>
               <Input type="password" required />
-              <Link
-                to="#"
-                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-              >
-                Forgot your password?
-              </Link>
             </div>
             <div className="flex flex-col gap-3">
               <Button type="submit" className="w-full">
-                Login
+                Submit
               </Button>
             </div>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link to="/sign-up" className="underline underline-offset-4">
-              Sign up
+            Already have an account?{" "}
+            <Link to="/login" className="underline underline-offset-4">
+              Login now
             </Link>
           </div>
         </form>
@@ -52,4 +62,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
