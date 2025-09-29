d4a91f320e49b46f1f6426a0d87553f19c4f0a07
delonlee01 <delonlee2001@hotmail.com>
2025-09-29 15:26:57 +0800

    Add reset password page UI

--- peerprep/src/pages/ResetPassword.tsx
+++ peerprep/src/pages/ResetPassword.tsx
@@ -10,12 +10,15 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router";
 
-const Login = () => {
+const ResetPassword = () => {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="text-center">
-        <CardTitle className="text-lg">Login to PeerPrep</CardTitle>
-        <CardDescription>Ace your interviews with PeerPrep</CardDescription>
+        <CardTitle className="text-lg">Reset Password</CardTitle>
+        <CardDescription>
+          We will send you a reset password link if the username or email is
+          valid
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <form>
@@ -24,26 +27,16 @@ const Login = () => {
               <Label htmlFor="email">Username or Email</Label>
               <Input type="text" required />
             </div>
-            <div className="grid gap-3">
-              <Label htmlFor="password">Password</Label>
-              <Input type="password" required />
-              <Link
-                to="/reset-password"
-                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-              >
-                Forgot your password?
-              </Link>
-            </div>
             <div className="flex flex-col gap-3">
               <Button type="submit" className="w-full">
-                Login
+                Submit
               </Button>
             </div>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link to="/sign-up" className="underline underline-offset-4">
-              Sign up
+            Remember your password?{" "}
+            <Link to="/login" className="underline underline-offset-4">
+              Login now
             </Link>
           </div>
         </form>
@@ -52,4 +45,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ResetPassword;
