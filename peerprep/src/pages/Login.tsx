682b48c4ff1864c184aff73981b2305a9799479a
delonlee01 <delonlee2001@hotmail.com>
2025-09-29 14:23:40 +0800

    Add login page UI

--- peerprep/src/pages/Login.tsx
+++ peerprep/src/pages/Login.tsx
@@ -1,5 +1,55 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader className="text-center">
+        <CardTitle className="text-lg">Login to PeerPrep</CardTitle>
+        <CardDescription>Ace your interviews with PeerPrep</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="email">Username or Email</Label>
+              <Input type="text" required />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" required />
+              <Link
+                to="#"
+                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </div>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link to="#" className="underline underline-offset-4">
+              Sign up
+            </Link>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Login;
