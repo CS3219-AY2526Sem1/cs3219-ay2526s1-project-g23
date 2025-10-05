2094676c7660015fc5b4829e8349c7d231c466c4
delonlee01 <delonlee2001@hotmail.com>
2025-10-05 12:10:12 +0800

    Update login form to send API request

--- peerprep/src/api/user-service.tsx
+++ peerprep/src/api/user-service.tsx
@@ -18,3 +18,22 @@ export const signUp = async (data: {
   });
   return message;
 };
+
+export const login = async (data: {
+  username?: string;
+  email?: string;
+  password: string;
+}) => {
+  const { token, user } = await userServiceRequest({
+    url: "/auth/login",
+    method: "post",
+    data,
+  });
+  const { id, username, email, isAdmin, stats } = user;
+  localStorage.setItem("jwtToken", token);
+  localStorage.setItem("userId", id);
+  localStorage.setItem("username", username);
+  localStorage.setItem("email", email);
+  localStorage.setItem("isAdmin", isAdmin);
+  localStorage.setItem("statistics", JSON.stringify(stats));
+};
--- peerprep/src/pages/Login.tsx
+++ peerprep/src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import { login } from "@/api/user-service";
+import Spinner from "@/components/custom/spinner";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,11 +8,61 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Link } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const formSchema = z.object({
+  usernameOrEmail: z.string().nonempty(),
+  password: z.string().nonempty(),
+});
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [loading, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      usernameOrEmail: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    startTransition(async () => {
+      try {
+        const formattedValues: {
+          email?: string;
+          username?: string;
+          password: string;
+        } = { password: values.password };
+        if (values.usernameOrEmail.includes("@")) {
+          formattedValues.email = values.usernameOrEmail;
+        } else {
+          formattedValues.username = values.usernameOrEmail;
+        }
+        await login(formattedValues);
+        navigate("/");
+      } catch (error) {
+        form.resetField("password");
+        form.setError("password", { type: "required" });
+        toast.error(error.message);
+      }
+    });
+  };
+
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="text-center">
@@ -18,35 +70,49 @@ const Login = () => {
         <CardDescription>Ace your interviews with PeerPrep</CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-3">
-              <Label htmlFor="email">Username or Email</Label>
-              <Input type="text" required />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="password">Password</Label>
-              <Input type="password" required />
-              <Link
-                to="#"
-                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-              >
-                Forgot your password?
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="usernameOrEmail"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username or Email</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <Link
+                    to="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </Link>
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full" disabled={loading}>
+              {loading && <Spinner />} Login
+            </Button>
+            <div className="text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link to="/sign-up" className="underline underline-offset-4">
+                Sign up
               </Link>
             </div>
-            <div className="flex flex-col gap-3">
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
-            </div>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link to="/sign-up" className="underline underline-offset-4">
-              Sign up
-            </Link>
-          </div>
-        </form>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
